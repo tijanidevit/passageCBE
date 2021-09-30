c5980c8b6315198f4348c3c64ace25be6966e7f5
TijaniDevIt <tijanidevit@yahoo.com>
2021-09-30 15:56:06 +0100

    first commit

---
+++ app/Http/Controllers/PassageController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Passage;
+use Illuminate\Http\Request;
+
+class PassageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Passage  $passage
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Passage $passage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Passage  $passage
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Passage $passage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Passage  $passage
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Passage $passage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Passage  $passage
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Passage $passage)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PassageQuestionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PassageQuestion;
+use Illuminate\Http\Request;
+
+class PassageQuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PassageQuestion  $passageQuestion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PassageQuestion $passageQuestion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PassageQuestion  $passageQuestion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PassageQuestion $passageQuestion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PassageQuestion  $passageQuestion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PassageQuestion $passageQuestion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PassageQuestion  $passageQuestion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PassageQuestion $passageQuestion)
+    {
+        //
+    }
+}
